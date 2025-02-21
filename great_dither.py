f6f3eccdefa5e0cb909fcd5991156a923f77eb5b
Michal Jenčo <michal.jenco.brno@gmail.com>
2025-02-21 01:46:27 +0100

    bahh

--- great_dither.py
+++ great_dither.py
@@ -7,7 +7,7 @@
 
 from PIL import Image, ImagePalette
 from time import time
-from math import  sin, tan, tanh, cos, sqrt
+from math import sin, tan, tanh, cos, sqrt
 
 from helper_functions import reduce_palette, generate_palette
 
@@ -21,27 +21,24 @@ def glitch_pixels(img: Image, func_r, func_g, func_b, base_wave_size: int) -> Im
 
             mod = int(h % 25)
 
-            r, g, b = fractally_func_4(r, g ,b, h, w, func_r, func_g, func_b, base_wave_size, mod, mod, mod)
+            r, g, b = fractally_func_4(r, g, b, h, w, func_r, func_g, func_b, base_wave_size, mod, mod, mod)
 
             pixels[w, h] = (r, g, b)
 
     return img
 
+
 def fractally_func_4(r, g, b, h, w, f1, f2, f3,
                      base_wave_size: int,
                      wave_size_modifier_r: int = 0,
                      wave_size_modifier_g: int = 0,
-                     wave_size_modifier_b: int = 0,
-                     i: int = 0) -> tuple:
+                     wave_size_modifier_b: int = 0) -> tuple:
     red_amount, green_amount, blue_amount = 235, 185, 215
 
-    # new_r = f1((b + w) / (base_wave_size + wave_size_modifier_r)) * red_amount
-    # new_g = f2((g + h + r) / (base_wave_size + wave_size_modifier_g)) * green_amount
-    # new_b = f3((r + w - h) / (base_wave_size + wave_size_modifier_b)) * blue_amount
-
-    new_r = f1((b + w) / 4 / (base_wave_size + wave_size_modifier_r)) * red_amount
-    new_g = f2((g + h + r) / 4 / (base_wave_size + wave_size_modifier_g)) * green_amount
-    new_b = f3((r + w - h) / 4 / (base_wave_size + wave_size_modifier_b)) * blue_amount
+    ### great highres dither
+    new_r = f1((b + sqrt(w) / 6) / (base_wave_size + wave_size_modifier_r)) * red_amount
+    new_g = f2((g + sqrt(h ** 2) / 13 + r) / (base_wave_size + wave_size_modifier_g)) * green_amount
+    new_b = f3((r + w / 11 - h) / (base_wave_size + wave_size_modifier_b)) * blue_amount
 
     return int(new_r), int(new_g), int(new_b)
 
@@ -50,9 +47,9 @@ def generate_consecutive_palettes(img: Image,
                                   image_count: int,
                                   base_wave_size: int,
                                   palette_size: int | None = None) -> Image:
-
     palette = generate_palette(size=palette_size)
-    for i in range(2, image_count + 2):
+    range_start = 2
+    for i in range(range_start, image_count + 2):
         ############# SWITCH BLOCK 1 and 2 FOR COOL EFFECT ################
 
         ############# BLOCK 2 ################
@@ -68,26 +65,28 @@ def generate_consecutive_palettes(img: Image,
                                   )
         ############# BLOCK 1 ################
 
-
         new_img = glitch_pixels(
             new_img,
-            base_wave_size = base_wave_size,
-            func_r = sin, func_g = tan, func_b = tanh,
+            base_wave_size=base_wave_size,
+            func_r=sin, func_g=tan, func_b=tanh,
         )
 
         img_save_name = f"pallette-out/6/{int(time())}-{i}.png"
-        new_img.save(img_save_name)
+
+        if i != range_start:
+            new_img.save(img_save_name)
 
 
 if __name__ == '__main__':
-    image = Image.open("source-imgs/momo1.jpg")
+    for i in range(0, 20):
+        image = Image.open("source-imgs/momo1.jpg")
 
-    pixels = image.load()
+        pixels = image.load()
 
-    generate_consecutive_palettes(image,
-                                  image_count=20,
-                                  palette_size=16,
+        generate_consecutive_palettes(image,
+                                      image_count=2,
+                                      palette_size=24,
 
-                                  # higher this number, better the dither becomes
-                                  base_wave_size=99,
-                                  )
+                                      # higher this number, better the dither becomes
+                                      base_wave_size=999,
+                                      )
--- readme.md
+++ readme.md
@@ -11,3 +11,5 @@ use the attached requirements.txt file to install the required packages
 run any of the example scripts, after that you're on your own
 
 ![ABSOLUTE CINEMA](example-out/1739923913-6.png)
+![ABSOLUTE CINEMA](example-out/1740097624-3.png)
+
