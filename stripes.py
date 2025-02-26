315a0858e427cbd47e96df54bb6b5b423d18a908
Michal Jenčo <michal.jenco.brno@gmail.com>
2025-02-27 00:32:48 +0100

    CRAZY good dither with text

--- stripes.py
+++ stripes.py
@@ -1,38 +1,55 @@
 # Copyright 2025
 # Author: Michal Jenčo
 # Email: michal.jenco.brno@gmail.com
+from random import randrange
 
-
-from PIL import Image
+from PIL import Image, ImageDraw, ImageFont
 import numpy as np
 from blend_modes import blending_functions
 
-from helper_functions import generate_palette, generate_stripes_overlay
+from helper_functions import generate_palette, generate_stripes_overlay, reduce_palette
 
 if __name__ == '__main__':
-    photo = Image.open("source-imgs/sky-sign.jpg")
+    photo = Image.open("source-imgs/momo2.jpg")
     w, h = photo.size
 
-    # draw.text(
-    #     xy=(w // 2 - 400, h // 2),
-    #     text="why do you hate me", font=ImageFont.truetype("arial", 150))
+    draw = ImageDraw.Draw(photo, mode="RGBA")
+
+    for i in range(0, 40):
+        draw.text(
+            xy=(randrange(0, w), randrange(0, h)),
+            text="Ur my boytoy now", font=ImageFont.truetype("arial", randrange(50, 80)))
 
     colors = generate_palette(8, group=True)
 
-    effects = generate_stripes_overlay(w, h, 20, 150, 5, colors)
 
     np_photo = np.array(photo)
     # append alpha channel
     np_photo = np.dstack((np_photo, np.ones((np_photo.shape[0], np_photo.shape[1], 1)) * 255))
     np_photo = np_photo.astype(float)
 
-    np_effect = np.array(effects)
-    # append alpha channel
-    np_effect = np_effect.astype(float)
 
-    imgOut = blending_functions.difference(np_photo, np_effect, 1.0)
+    for i in range(0, 1):
+        palIm = Image.new('P', (1, 1))
+        palette = generate_palette(8)
+        palIm.putpalette(palette)
+        effects = generate_stripes_overlay(w, h, 20, 150, 15, colors)
+        np_effect = np.array(effects)
+        # append alpha channel
+        np_effect = np_effect.astype(float)
 
-    # Save images
-    imgOut = np.uint8(imgOut)
-    imgOut = Image.fromarray(imgOut)
-    imgOut.show()
+        imgOut = blending_functions.difference(np_photo, np_effect, 1.0)
+
+        # Save images
+        imgOut = np.uint8(imgOut)
+        imgOut = Image.fromarray(imgOut)
+
+        imgOut = reduce_palette(8, imgOut, palette)
+        imgOut = imgOut.convert("L")
+        imgOut = photo.resize(size=(w // 8, h // 8), resample=0)
+        # and scale it up to get pixelate effect
+        imgOut = photo.resize((w, h), resample=0)
+        imgOut = imgOut.quantize(palette=palIm, dither=Image.Dither.FLOYDSTEINBERG)
+
+        imgOut.show()
+        imgOut.save(f"asd-{i}.png")
