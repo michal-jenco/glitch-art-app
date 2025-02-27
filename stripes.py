f0610cb617a340f3a225c5368628dae2faa65535
Michal Jenčo <michal.jenco.brno@gmail.com>
2025-02-27 01:04:00 +0100

    wahh

--- stripes.py
+++ stripes.py
@@ -6,36 +6,39 @@ from random import randrange
 from PIL import Image, ImageDraw, ImageFont
 import numpy as np
 from blend_modes import blending_functions
+from time import time
 
 from helper_functions import generate_palette, generate_stripes_overlay, reduce_palette
 
+
 if __name__ == '__main__':
-    photo = Image.open("source-imgs/momo2.jpg")
+    photo = Image.open("source-imgs/muse.jpg")
     w, h = photo.size
 
     draw = ImageDraw.Draw(photo, mode="RGBA")
 
-    for i in range(0, 40):
-        draw.text(
-            xy=(randrange(0, w), randrange(0, h)),
-            text="Ur my boytoy now", font=ImageFont.truetype("arial", randrange(50, 80)))
-
-    colors = generate_palette(8, group=True)
 
 
-    np_photo = np.array(photo)
-    # append alpha channel
-    np_photo = np.dstack((np_photo, np.ones((np_photo.shape[0], np_photo.shape[1], 1)) * 255))
-    np_photo = np_photo.astype(float)
 
+    for i in range(0, 100):
+        for i in range(0, 5):
+            draw.text(
+                xy=(randrange(-500, w), randrange(0, h)),
+                text="do you ever think about me?", font=ImageFont.truetype("arial", randrange(15, 58)))
 
-    for i in range(0, 1):
+        colors = generate_palette(8, group=True)
+
+        np_photo = np.array(photo)
+        # append alpha channel
+        np_photo = np.dstack((np_photo, np.ones((np_photo.shape[0], np_photo.shape[1], 1)) * 255))
+        np_photo = np_photo.astype(float)
+
         palIm = Image.new('P', (1, 1))
-        palette = generate_palette(8)
+        palette = generate_palette(16)
         palIm.putpalette(palette)
+
         effects = generate_stripes_overlay(w, h, 20, 150, 15, colors)
         np_effect = np.array(effects)
-        # append alpha channel
         np_effect = np_effect.astype(float)
 
         imgOut = blending_functions.difference(np_photo, np_effect, 1.0)
@@ -49,7 +52,6 @@ if __name__ == '__main__':
         imgOut = photo.resize(size=(w // 8, h // 8), resample=0)
         # and scale it up to get pixelate effect
         imgOut = photo.resize((w, h), resample=0)
-        imgOut = imgOut.quantize(palette=palIm, dither=Image.Dither.FLOYDSTEINBERG)
+        imgOut = imgOut.quantize(palette=palIm, dither=Image.Dither.RASTERIZE)
 
-        imgOut.show()
-        imgOut.save(f"asd-{i}.png")
+        imgOut.save(f"pallette-out/text1/text1-{time()}-{i}.png")
