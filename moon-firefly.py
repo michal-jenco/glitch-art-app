d8c1821380382778230966f91fa9a2f313026f49
Michal Jenčo <michal.jenco.brno@gmail.com>
2025-03-03 17:15:11 +0100

    finished

--- moon-firefly.py
+++ moon-firefly.py
@@ -16,27 +16,33 @@ if __name__ == '__main__':
     draw = ImageDraw.Draw(photo, mode="RGBA")
     w, h = photo.size
 
-    num_of_frames = 70
-    palette_size = 16
+    num_of_frames = 28 * 4
+    palette_size = 14
     words = "this", "is", "how", "I", "see", "you", "btw"
     word_count = len(words)
     col_width = w // word_count
 
     assert num_of_frames >= word_count
 
+    starting_y = 30
+
     for i in range(num_of_frames):
         word_idx = i // (num_of_frames // word_count)
         starting_x_pos = word_idx * col_width
 
-        x = int(randint(starting_x_pos, col_width + starting_x_pos))
-        y = int(randint(0, h))
+        ### random column positions algo
+        # x = int(randint(starting_x_pos, col_width + starting_x_pos))
+        # y = int(randint(0, h))
+        ### random column positions algo
 
+        x = int(randint(starting_x_pos, col_width + starting_x_pos))
+        y = (starting_y + i * 30) % h
         text = words[word_idx]
 
         draw.text(
             xy=(x, y),
             text=text,
-            font=ImageFont.truetype("arial", randint(20, 40)),
+            font=ImageFont.truetype("arial", randint(10, 40)),
         )
 
         palIm = Image.new('P', (1, 1))
@@ -45,4 +51,4 @@ if __name__ == '__main__':
 
         imgOut = photo.quantize(palette=palIm, dither=Image.Dither.ORDERED)
 
-        imgOut.save(f"pallette-out/moon-how-i-see-you/frames/moon-{int(time())}-frame{i}.png")
+        imgOut.save(f"pallette-out/moon-how-i-see-you/frames2/moon-{int(time())}-frame{i}.png")
