8a26e71bd1bc42577c01468c6397672c50c0312c
Michal Jenčo <michal.jenco.brno@gmail.com>
2025-03-03 16:51:14 +0100

    random algo based on columns finished!!

--- moon-firefly.py
+++ moon-firefly.py
@@ -4,28 +4,45 @@
 
 
 from PIL import Image, ImageFont, ImageDraw
-import numpy as np
 from time import time
+
+from numpy.random import randint
 
 from helper_functions import generate_palette
 
 
 if __name__ == '__main__':
     photo = Image.open("source-imgs/moon.jpg")
+    draw = ImageDraw.Draw(photo, mode="RGBA")
     w, h = photo.size
 
-    palette_size = 4
-    pixelate_level = 4
+    num_of_frames = 70
+    palette_size = 16
+    words = "this", "is", "how", "I", "see", "you", "btw"
+    word_count = len(words)
+    col_width = w // word_count
 
-    draw = ImageDraw.Draw(photo, mode="RGBA")
-    draw.text(
-        xy=(w // 2 - 400, h // 2),
-        text="this is how I see you btw", font=ImageFont.truetype("arial", 25))
+    assert num_of_frames >= word_count
 
-    palIm = Image.new('P', (1, 1))
-    palette = generate_palette(palette_size)
-    palIm.putpalette(palette)
+    for i in range(num_of_frames):
+        word_idx = i // (num_of_frames // word_count)
+        starting_x_pos = word_idx * col_width
 
-    imgOut = photo.quantize(palette=palIm, dither=Image.Dither.FLOYDSTEINBERG)
+        x = int(randint(starting_x_pos, col_width + starting_x_pos))
+        y = int(randint(0, h))
 
-    imgOut.save(f"pallette-out/moon-how-i-see-you/moon-{int(time())}.png")
+        text = words[word_idx]
+
+        draw.text(
+            xy=(x, y),
+            text=text,
+            font=ImageFont.truetype("arial", randint(20, 40)),
+        )
+
+        palIm = Image.new('P', (1, 1))
+        palette = generate_palette(palette_size)
+        palIm.putpalette(palette)
+
+        imgOut = photo.quantize(palette=palIm, dither=Image.Dither.ORDERED)
+
+        imgOut.save(f"pallette-out/moon-how-i-see-you/frames/moon-{int(time())}-frame{i}.png")
