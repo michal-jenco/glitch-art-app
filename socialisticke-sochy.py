ee1b68e6315a96c56cc85438454662c559ede612
Michal Jenčo <michal.jenco.brno@gmail.com>
2025-03-04 20:40:02 +0100

    boah

--- socialisticke-sochy.py
+++ socialisticke-sochy.py
@@ -30,10 +30,10 @@ def iterate_pixels(img_orig, img_palette, threshold: int):
 
 if __name__ == '__main__':
     for i in range(10):
-        photo = Image.open("source-imgs/sidliskove-sochy/best/10_1.jpg")
+        photo = Image.open("source-imgs/sidliskove-sochy/best/1.jpg")
         w, h = photo.size
 
-        palette_size = 8
+        palette_size = 7
         pixel_amt = 8
 
         np_photo = np.array(photo)
@@ -49,7 +49,7 @@ if __name__ == '__main__':
         np_palette = np.array(imgOut)
         np_palette = np_palette.astype(float)
 
-        combined = iterate_pixels(np_photo, np_palette, threshold=80)
+        combined = iterate_pixels(np_photo, np_palette, threshold=110)
         combined_image = Image.fromarray(np.array(combined))
         combined_image.save(f"pallette-out/sidliskove-sochy/{time()}.png")
 
