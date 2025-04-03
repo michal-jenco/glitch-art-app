63828596a36b04aa88d56fe9ddc63d66284946bf
Michal Jenčo <michal.jenco.brno@gmail.com>
2025-04-03 19:26:19 +0200

    19999 peak

--- 19999.py
+++ 19999.py
@@ -1,10 +1,9 @@
 # Copyright 2025
 # Author: Michal Jenčo
 # Email: michal.jenco.brno@gmail.com
-
+from random import randrange
 
 from PIL import Image
-import numpy as np
 from time import time
 from pathlib import Path
 
@@ -18,19 +17,20 @@ if __name__ == '__main__':
     output_imgs_folder = f"pallette-out/{proj_name}"
 
     input_image_paths = list(Path(input_imgs_folder).glob("*.jpg"))
-    palette_size = 4
+    palette_size = 8
 
     palette = generate_palette(palette_size)
+    upscale_x = 3
 
-    for i, img_path in (enumerate(input_image_paths)):
+    for i, img_path in (enumerate(input_image_paths[140:-100])):
         glitched_image = Image.open(img_path)
         w, h = glitched_image.size
 
         ### ADD STRIPE COPIES
         create_stripes(
             glitched_image,
-            num_of_stripes=(i % 20),
-            stripe_height=((i * 5) % 15) + 7,
+            num_of_stripes=randrange(5, 10),
+            stripe_height=randrange(5, 20),
         )
         ### ADD STRIPE COPIES
 
@@ -44,6 +44,7 @@ if __name__ == '__main__':
         )
         ### DITHER + PALETTE
 
-        glitched_image.save(f"pallette-out/{proj_name}/{img_path.stem}-{time()}.png")
+        glitched_image = glitched_image.resize((w * upscale_x, h * upscale_x), resample=0)
+        glitched_image.save(f"pallette-out/{proj_name}/2/{img_path.stem}-{time()}.png")
 
         palette = vary_palette(palette, 20, 10, 30)
