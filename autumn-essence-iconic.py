f27f28798d55193d8e8479415e4c82a664d21fa1
Michal Jenčo <michal.jenco.brno@gmail.com>
2025-04-12 15:36:24 +0200

    the essence of autumn pt 2

--- autumn-essence-iconic.py
+++ autumn-essence-iconic.py
@@ -5,10 +5,11 @@
 
 from PIL import Image, ImageFont, ImageDraw
 from time import time
+from random import randrange
 
 from numpy.random import randint
 
-from helper_functions import generate_palette
+from helper_functions import create_stripes, generate_palette
 
 
 if __name__ == '__main__':
@@ -24,7 +25,6 @@ if __name__ == '__main__':
     assert num_of_frames >= word_count
 
     starting_y = 30
-    palette = generate_palette(12)
 
     for i in range(num_of_frames):
         word_idx = i // (num_of_frames // word_count)
@@ -43,4 +43,26 @@ if __name__ == '__main__':
             stroke_fill='white'
         )
 
-        photo.save(f"pallette-out/autumn-essence/{int(time())}-frame{i}.png")
+        if True:
+            photocopy = photo.copy()
+            photocopy = photocopy.convert("RGB")
+
+            ### ADD STRIPE COPIES
+            create_stripes(
+                photocopy,
+                num_of_stripes=randrange(5, 15),
+                stripe_height=randrange(5, 50),
+            )
+            ### ADD STRIPE COPIES
+
+            palette = generate_palette(4)
+            palette_image = Image.new('P', (1, 1))
+            palette_image.putpalette(palette)
+
+            photocopy = photocopy.quantize(
+                palette=palette_image,
+                dither=Image.Dither.FLOYDSTEINBERG,
+            )
+
+            photocopy = photocopy.convert("RGB")
+            photocopy.save(f"pallette-out/autumn-essence/{int(time())}-frame{i}.jpg")
